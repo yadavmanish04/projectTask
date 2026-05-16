8d0fcfd4001f1cdedd249cae26675163ff775af0
Manishydv <manishyadav11406@gmail.com>
2026-05-16 10:57:51 +0530

    fix cors issue

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import { storage } from '../utils/storage';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || '/api',
-  withCredentials: false,
+  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
